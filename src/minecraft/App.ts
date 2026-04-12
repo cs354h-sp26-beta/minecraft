d1863e0814d6610f649d3c367d0f79bd8396f4fc
Michael Goppert <mi@gopp.dev>
2026-04-11 19:03:09 -0500

    bugfix: Render distance; cleanup: Remove chunk-local seed infra

--- src/minecraft/App.ts
+++ src/minecraft/App.ts
@@ -16,13 +16,10 @@ import { Player } from "./Entity.js";
 export class MinecraftAnimation extends CanvasAnimation {
   private gui: GUI;
 
-  // TODO: Map chunk to seed!
-  private chunkSeeds: Map<string, string>;
-
   private chunkCache: LruCache<string, Chunk>;
   private renderedChunks: Map<string, Chunk>;
 
-  private static readonly renderDistance: number = 3;
+  private static readonly renderDistance: number = 1;
 
   /*  Cube Rendering */
   private cubeGeometry: Cube;
@@ -45,7 +42,6 @@ export class MinecraftAnimation extends CanvasAnimation {
     const gl = this.ctx;
 
     this.gui = new GUI(this.canvas2d, this);
-    this.chunkSeeds = new Map();
     this.chunkCache = new LruCache();
     this.renderedChunks = new Map();
     const playerPosition = this.gui.getCamera().pos();
@@ -172,14 +168,6 @@ export class MinecraftAnimation extends CanvasAnimation {
     return this.renderedChunks.get(`${chunkX},${chunkZ}`)!;
   }
 
-  // Initializes a chunk and maps its seed.
-  private initChunk(chunkX: number, chunkZ: number): Chunk {
-    const chunk = new Chunk(chunkX, chunkZ, 64);
-    const key = `${chunkX},${chunkZ}`;
-    this.chunkSeeds.set(key, chunk.seed);
-    return chunk;
-  }
-
   // Given a location (we encode this as a string for now) and a seed, reconstruct the original chunk.
   //
   // FIXME: Maybe this should be in `Chunk`, but only allowed a single constructor.
@@ -207,18 +195,7 @@ export class MinecraftAnimation extends CanvasAnimation {
         const chunkZ = cz + dj * 64;
         const key = `${chunkX},${chunkZ}`;
         if (!this.chunkCache.has(key)) {
-          const chunkToLoadSeed = this.chunkSeeds.get(key);
-          if (chunkToLoadSeed === undefined) {
-            const initChunk = this.initChunk(chunkX, chunkZ);
-            this.chunkCache.set(key, initChunk);
-          } else {
-            const loadedChunk = this.loadChunkFromSeed(
-              chunkX,
-              chunkZ,
-              chunkToLoadSeed,
-            );
-            this.chunkCache.set(key, loadedChunk);
-          }
+          this.chunkCache.set(key, new Chunk(chunkX, chunkZ, 64));
         }
         const cachedChunk = this.chunkCache.get(key)!;
         this.renderedChunks.set(key, cachedChunk);
@@ -246,8 +223,11 @@ export class MinecraftAnimation extends CanvasAnimation {
    *
    */
   public draw(): void {
+    // Load chunks.
+    this.loadChunksAroundPlayer();
+
     // To slow movement to something more natural, scale the amount we can move per frame.
-    const dt = 1;
+    const dt = 1 / 60;
 
     const walkDx = this.gui.walkDir().scale(dt, new Vec3());
     const momentumDx = this.player.velocity.scale(dt, new Vec3());
@@ -275,9 +255,6 @@ export class MinecraftAnimation extends CanvasAnimation {
       this.player.velocity = v;
       this.player.position.y = floorY + Player.hitboxHeight;
     }
-
-    this.loadChunksAroundPlayer();
-
     // Drawing
     const gl: WebGLRenderingContext = this.ctx;
     const bg: Vec4 = this.backgroundColor;
