2670f554ea4e1ec076dfdbd6694003b5dad5b512
Danny Youstra <danny@youstra.com>
2026-04-16 12:56:32 -0500

    update submission script

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -9,6 +9,9 @@ mkdir -p minecraft
 cp -r src minecraft/
 cp make-minecraft.py minecraft/
 cp README.md minecraft/
+cp biome.json minecraft/
+cp project-description.pdf minecraft/
+
 
 # Make archive and clean up.
 tar --exclude='._*' -czvf $1.tgz minecraft/
